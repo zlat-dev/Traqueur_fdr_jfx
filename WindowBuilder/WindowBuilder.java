a41769989b0edff989a6f4594347e6b6d553a2a9
zlat-dev <zlat@club.fr>
2025-01-05 12:34:28 +0100

    20250105 Injection window builder et approche sans fxml + controller

--- WindowBuilder/WindowBuilder.java
+++ WindowBuilder/WindowBuilder.java
@@ -1,25 +1,16 @@
-package hellofx;
-
-// import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class WindowBuilder extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        // Parent root = FXMLLoader.load(getClass().getResource("hellofx.fxml"));
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("My Awesome Window");
 
         Pane pane = new Pane();
         pane.setPrefSize(864, 432);
@@ -31,24 +22,27 @@ public class Main extends Application {
         element1.setPrefWidth(105.81);
         element1.setPrefHeight(29.00);
         element1.setDisable(false);
-        element1.setFont(Font.loadFont(getClass().getResourceAsStream("resources/fonts/Lato.ttf"), 14.00));
+        element1.setFont(Font.loadFont(getClass().getResourceAsStream("/resources/fonts/Lato.ttf"), 14.00));
         element1.setStyle("-fx-background-color: #2e2e2e; -fx-text-fill: #D9D9D9; -fx-border-color: #979797; -fx-border-radius: 4px; -fx-background-radius: 4px; -fx-border-width: 1px;");
         element1.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> { element1.setBackground(new Background(new BackgroundFill(Color.web("#232323"), new CornerRadii(4.00), null))); });
         element1.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> { element1.setBackground(new Background(new BackgroundFill(Color.web("#2e2e2e"), new CornerRadii(4.00), null))); });
         pane.getChildren().add(element1);
 
+        TextArea element2 = new TextArea("test");
+        element2.setLayoutX(64.57);
+        element2.setLayoutY(103.19);
+        element2.setPrefWidth(105.81);
+        element2.setPrefHeight(41.00);
+        element2.setPromptText("Your long Input!");
+        element2.setFont(Font.loadFont(getClass().getResourceAsStream("/resources/fonts/Lato.ttf"), 14.00));
+        element2.setStyle("-fx-control-inner-background: #B2B2B2; -fx-background-color: #B2B2B2; -fx-text-fill: #353535; -fx-border-color: #979797; -fx-border-width: 0px; -fx-border-radius: 2px; -fx-prompt-text-fill: #656565;");
+        pane.getChildren().add(element2);
+
         Scene scene = new Scene(pane, 864, 432);
-
-        primaryStage.setTitle("Hello World");
-
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        
-        // primaryStage.setScene(new Scene(root, 400, 300));
-        // primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
